8c29174976e78590abbbc1ba93e235373bbd1778
JSalvatierrajkl <joeljsalvatierra@gmail.com>
2023-09-29 19:04:40 -0300

    jkl

--- WDD230/scripts/getdates.js
+++ WDD230/scripts/getdates.js
@@ -5,3 +5,12 @@ document.getElementById('currentYear').textContent = currentYear;
 // Get the last modified date and populate it in the second paragraph
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+// Make the hamburguer menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
